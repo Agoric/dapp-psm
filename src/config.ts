b20b88da96433bfaab067ad509c7d3fc243f8c80
samsiegart <samsiegart@gmail.com>
2022-10-19 10:33:01 -0700

    chore: add emerynet

--- src/config.ts
+++ src/config.ts
@@ -21,6 +21,10 @@ export const networkConfigs = {
     label: 'Agoric Ollinet',
     url: 'https://ollinet.agoric.net/network-config',
   },
+  emerynet: {
+    label: 'Agoric Emerynet',
+    url: 'https://emerynet.agoric.net/network-config',
+  },
   localhost: {
     label: 'Local Network',
     url: 'http://localhost:3000/wallet/network-config',
